0fb43927887374126ba93d1320b3c2f4c8340fe3
EdwardPai1123 <edwardsamulpai@gmail.com>
2025-02-13 23:44:27 -0800

    Need to debug

--- main.cpp
+++ main.cpp
@@ -50,7 +50,15 @@ public:
         head = newNode;
         length++;
     }
-
+    Node<T> *get(int index) {
+        if (index < 0 || index >= length)
+            return nullptr;
+        Node<T> * temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp -> next;
+            return temp;
+        }
+    }
     void delfirst() {
         Node<T> *temp = head;
         head = head->next;
@@ -68,14 +76,62 @@ public:
 
     void deleteNode(int index) {
        //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
+        if (index < 0 || index >= length) {
+            cout <<"index is deleted" << endl;
+            return;
+        }
+        if (index == 0) {
+            delfirst();
+        }
+        if (index == length -1) {
+            dellast();
+        }
+        else {
+            Node<T> * temp = get(index - 1);
+            Node<T> *toDelete = temp -> next;
+            temp->next = toDelete->next;
+            delete toDelete;
+            length--;
+        }
+        return;
     }
 
    void insert(int index, T *value) {
         //TODO:Write a function to insert a new node at a give index. Reuse the pre-written functions for edge cases. Account for missing index
+        if (index < 0 || index >= length) {
+            cout<<"index is invalid"<<endl;
+            return;
+        }
+        if (index == 0) {
+            addhead(value);
+        }
+        if (index == length) {
+            add(value);
+        }
+        else {
+            Node<T> *newNode = new Node<T>(value);
+            Node<T> * temp = get(index - 1);
+            newNode-next=temp->next;
+            temp->next = newNode;
+            length++;
+        }
+        return;
     }
 
    void reverselist(){
         //TODO:Write a function to reverse the list using the logic from the slide.
+        Node<T>* prev = NULL;
+        Node<T>* next = NULL;
+        Node<T>* curr = head;
+
+        while(curr!=NULL)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     void print() {
@@ -100,4 +156,10 @@ int main() {
     ll->print();
     ll->dellast();
     ll->print();
+    ll->deleteNode(1);
+    ll->print();
+    ll->reverselist();
+    ll->print();
+
+    return 0;
 }
